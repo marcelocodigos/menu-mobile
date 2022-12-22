dc8c7888e9275d516fe32d84664421bf792f2780
marcelocodigos <marcelofdsantoscode@gmail.com>
2022-12-22 14:00:18 -0300

    add funcionlaidade ao SubMenu de Outros

--- js-menu-mobile/js-menu-mobile.js
+++ js-menu-mobile/js-menu-mobile.js
@@ -1,9 +1,20 @@
 let navMenu=document.getElementById("menu-hamburger");
+let subMenuItem=document.getElementById("sub-menu-item");
 
 toogleMenu=()=>{
     let navegacao=document.getElementById("nav");
     navegacao.classList.toggle("menu-ativo");
-    
+
 }
 
+toogleSubMenuItem=()=>{
+   subMenuItem.classList.toggle("subMenu-item-ativo");
+   if(document.querySelector('li.subMenu-item-ativo') !== null){
+    let subItens=document.getElementById('sub-menu-items').style.display='block';
+ 
+   }else{
+    document.getElementById('sub-menu-items').style.display='none';
+   }
+}
 navMenu.addEventListener("click",toogleMenu);
+subMenuItem.addEventListener("click",toogleSubMenuItem);
